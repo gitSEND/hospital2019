b488bd89b3c224d4e80c0c846ad496002c20a570
NelsonDSanchezE <n.sanchezesparza@gmail.com>
2019-10-13 18:38:14 -0500

    Creación del proyecto

---
+++ src/main/java/com/salud/gestion/ProyectoSalud2019Application.java
@@ -0,0 +1,13 @@
+package com.salud.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSalud2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSalud2019Application.class, args);
+	}
+
+}
